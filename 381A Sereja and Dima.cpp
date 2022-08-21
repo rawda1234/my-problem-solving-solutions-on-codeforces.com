0c4578f6abee4fa766dc97df8b46c67a5ee9126c
rawda1234 <108896808+rawda1234@users.noreply.github.com>
2022-08-21 23:12:20 +0200

    Update 381A Sereja and Dima.cpp

--- 381A Sereja and Dima.cpp
+++ 381A Sereja and Dima.cpp
@@ -1,48 +1,26 @@
-//problem link:http://codeforces.com/contest/381/problem/A
-//status:accepted
 #include <iostream>
+#include<algorithm>
 using namespace std;
-int main() {
-int n;
-cin>>n;
-int arr[n];
-for(int i=0;i<n;i++){
-	cin>>arr[i];
-}
-int r=n-1;
-int l=0;
-int sscore=0;
-int dscore=0;
-for(int i=0;i<n;i++){
-	if(i%2==0){
-		if(arr[r]>arr[l])
-		{
-			sscore+=arr[r];
-			--r;
-		}
-		else if(arr[r]<arr[l]){
-		 sscore+=arr[l];
-			++l;
-		}
-		else{
-		sscore+=arr[l];	
-		}
+int arr[10001];
+int main()
+{
+
+	int n = 0;
+	int sum1 = 0, sum2 = 0;
+	cin >> n; 
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
 	}
-	else{
-		if(arr[r]>arr[l])
-		{
-			dscore+=arr[r];
-			--r;
-		}
-		else if(arr[r]<arr[l]){
-		 dscore+=arr[l];
-			++l;
-		}
-			else{
-		dscore+=arr[l];	
-		}
-		}
-}
-cout<<sscore<<" "<<dscore;
+	sort(arr, arr + n);
+	for (int i = (n - 1); i >= 0; i -= 2)
+	{
+		sum1 += arr[i];  
+	}
+	for (int i = (n - 2); i >= 0; i -= 2)
+	{
+		sum2 += arr[i];
+	}
+	cout << sum1 << " " << sum2 << endl;
 	return 0;
 }
